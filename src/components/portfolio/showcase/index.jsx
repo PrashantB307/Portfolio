326c7bbb68e2ee1b8fb99568c5ca3675cc1143cb
PrashantB-307 <pb28.rkt@gmail.com>
2024-03-20 23:57:53 +0530

    :recycle: Showcase :recycle:

---
+++ src/components/portfolio/showcase/index.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import "./style.scss";
+import Arrow from "../../shared/arrow";
+
+const Showcase = ({ data, transition }) => {
+    return (
+        <div className="projects-showcase" >
+            {data.map((project) => (
+                <div
+                    className={`showcase-item ${
+                        transition === "zoomout"
+                            ? "zoomOut"
+                            : transition === "zoomin"
+                            ? "zoomIn"
+                            : ""
+                    }`}
+                    key={project.id}
+                >
+                    
+                    <a href={project.link}>
+                        <div className="meta-content">    
+                            <h3>{project.name}</h3>
+                            <link rel="stylesheet" href={project.link} />
+                            <div className="go-to-cta"  >
+                                <span className="text">See Project</span>
+                                <Arrow />
+                            </div>
+                        </div>
+                    </a>
+                    <img src={project.media.thumbnail} alt="" />
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default Showcase;
